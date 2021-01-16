a0ad832c2feb783c527017ac89fba4ac916996dc
Katy La Tour <klatour324@gmail.com>
2021-01-16 16:07:08 -0600

    Pass encrypt test with main functionality built for encrypt method

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -12,10 +12,10 @@ class EnigmaTest < Minitest::Test
     enigma = Enigma.new
 
     expected = {
-                  encryption: "keder ohulw",
-                  key: "02715",
-                  date: "040895"
-                }
+                encryption: "keder ohulw",
+                key: "02715",
+                date: "040895"
+               }
 
     assert_equal expected, enigma.encrypt("hello world", "02715", "040895")
   end
