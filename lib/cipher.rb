c6a2009ad507417d614f157f0b3506f0c555ed51
Katy La Tour <klatour324@gmail.com>
2021-01-16 22:51:20 -0600

    Refactor offset test and create reader method for offset

--- lib/cipher.rb
+++ lib/cipher.rb
@@ -4,13 +4,15 @@ class Cipher
   attr_reader :a_key,
               :b_key,
               :c_key,
-              :d_key
+              :d_key,
+              :offset
 
   def initialize
     @a_key = nil
     @b_key = nil
     @c_key = nil
     @d_key = nil
+    @offset = nil
   end
 
   def create_keys(key)
@@ -23,20 +25,19 @@ class Cipher
   end
 
   def create_offset(date)
-    ((date.to_i) ** 2).to_s.split('').last(4)
+    @offset = ((date.to_i) ** 2).to_s.split('').last(4)
   end
 
   def encrypt(message, key, date)
-   offset = create_offset(date)
-   create_keys(key)
+    create_offset(date)
+    create_keys(key)
 
-   a_shift = a_key + offset[0].to_i
-   b_shift = b_key + offset[1].to_i
-   c_shift = c_key + offset[2].to_i
-   d_shift = d_key + offset[3].to_i
+    a_shift = a_key + offset[0].to_i
+    b_shift = b_key + offset[1].to_i
+    c_shift = c_key + offset[2].to_i
+    d_shift = d_key + offset[3].to_i
 
-
-   shifts = [a_shift, b_shift, c_shift, d_shift]
+    shifts = [a_shift, b_shift, c_shift, d_shift]
 
     encrypted_message = message.downcase.split('').each_with_index.reduce("") do |memo, (char, index)|
       shift_index = index % shifts.length
--- test/cipher_test.rb
+++ test/cipher_test.rb
@@ -12,18 +12,20 @@ class CipherTest < Minitest::Test
     cipher = Cipher.new
 
     expected = {
-                encryption: "keder ohulw",
-                key: "02715",
-                date: "040895"
+                encryption: 'keder ohulw',
+                key: '02715',
+                date: '040895'
                }
 
-    assert_equal expected, cipher.encrypt("hello world", "02715", "040895")
+    assert_equal expected, cipher.encrypt('hello world', '02715', '040895')
   end
 
   def test_it_can_create_an_offset
     cipher = Cipher.new
 
-    assert_equal ['4', '7', '2', '1'], cipher.create_offset('180689')
+    cipher.create_offset('180689')
+
+    assert_equal ['4', '7', '2', '1'], cipher.offset
   end
 
   def test_it_can_create_keys
@@ -36,4 +38,21 @@ class CipherTest < Minitest::Test
     assert_equal 71, cipher.c_key
     assert_equal 15, cipher.d_key
   end
+
+  # def test_it_can_make_shifts
+  #   cipher = Cipher.new
+  #   a_shift = 3
+  #   b_shift = 27
+  #   c_shift = 73
+  #   d_shift = 20
+  #
+  #   cipher.create_offset('040895')
+  #   cipher.create_keys('02715')
+  #   cipher.make_shifts
+  #
+  #   assert_equal 3, cipher.a_shift
+  #   assert_equal 27, cipher.b_shift
+  #   assert_equal 73, cipher.c_shift
+  #   assert_equal 20, cipher.d_shift
+  # end
 end
