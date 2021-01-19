654beb73a398914cec0e9b561f6e580e94cd6c16
Katy La Tour <klatour324@gmail.com>
2021-01-18 23:02:09 -0600

    Add Key class and methods to create keys and generate random keys

--- lib/key.rb
+++ lib/key.rb
@@ -1,4 +1,21 @@
-module Key
+class Key
+  attr_reader :key
+
+  def initialize(key)
+    @key = key ||= generate_key
+  end
+
+  def create_keys
+    split_key = key.split('')
+
+    a_key = (split_key[0] + split_key[1])
+    b_key = (split_key[1] + split_key[2])
+    c_key = (split_key[2] + split_key[3])
+    d_key = (split_key[3] + split_key[4])
+
+    [a_key, b_key, c_key, d_key]
+  end
+
   def generate_key
     rand(9999).to_s.rjust(5, '0')
   end
